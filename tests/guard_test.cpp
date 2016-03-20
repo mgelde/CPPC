37e699bd28e7e736db71542b1676e39bac4c12f4
Marcus <redmnic@gmail.com>
2016-03-20 17:21:12 +0100

    tests: Write a mocking API.
    To have meaningful unit tests, extend the demo API to provide callable
    objects whose call-status can be queried.

--- tests/guard_test.cpp
+++ tests/guard_test.cpp
@@ -7,69 +7,55 @@
 #include "test_api.h"
 
 using namespace cwrap::guard;
-
-class GuardTest : public testing::Test {
-    public:
-        void SetUp() override;
-        void TearDown() override;
-
-        static bool _called;
-};
-
-bool GuardTest::_called { false };
-
-void GuardTest::SetUp() {
-    _called = false;
-}
+using namespace cwrap::testing::mock;
+using namespace cwrap::testing::assertions;
 
 template <class T>
 struct CustomDeleter {
     void operator() (T &t) const {
-        GuardTest::_called = true;
-        (void) t;
+        release_resources(&t);
     }
 };
 
-void GuardTest::TearDown() {
-}
+class GuardTest : public ::testing::Test {
+    public:
+        void SetUp() override {
+            MockAPI::instance().reset();
+        }
+};
 
 TEST_F(GuardTest, testPassingPointerToAllocatedMemory) {
     {
         auto freeFunc = [this](some_type_t &ptr) {
-            ptr.x = 2222;
-            ptr.data = nullptr;
-            some_free_func(&ptr);
-            _called = true;
+            release_resources(&ptr);
         };
-        ASSERT_FALSE(_called);
         Guard<some_type_t, decltype(freeFunc)> guard {freeFunc};
-        ASSERT_FALSE(_called);
+        ASSERT_NOT_CALLED(MockAPI::instance().releaseResourcesFunc());
         do_init_work(&guard.get());
-        ASSERT_FALSE(_called);
+        ASSERT_NOT_CALLED(MockAPI::instance().releaseResourcesFunc());
     }
-    ASSERT_TRUE(_called);
+    ASSERT_CALLED(MockAPI::instance().releaseResourcesFunc());
 }
 
 TEST_F(GuardTest, testInitFunctionReturningPointer) {
     {
         auto freeFunc = [this](some_type_t *&ptr) {
-            _called = true;
-            some_free_func(ptr);
+            free_resources(ptr);
         };
-        auto guard = make_guarded<some_type_t*, decltype(freeFunc)>(freeFunc, do_init_work_return());
-        ASSERT_FALSE(_called);
+        auto guard = make_guarded<some_type_t*, decltype(freeFunc)>(freeFunc, create_and_initialize());
+        ASSERT_FALSE(guard.get()->isFreed());
+        ASSERT_NOT_CALLED(MockAPI::instance().freeResourcesFunc());
     }
-    ASSERT_TRUE(_called);
+    ASSERT_CALLED(MockAPI::instance().freeResourcesFunc());
 }
 
 
 TEST_F(GuardTest, testWithCustomDeleter) {
     {
-        ASSERT_FALSE(_called);
         Guard<some_type_t, CustomDeleter<some_type_t>> guard {};
+        ASSERT_NOT_CALLED(MockAPI::instance().releaseResourcesFunc());
         do_init_work(&guard.get());
-        ASSERT_FALSE(_called);
+        ASSERT_NOT_CALLED(MockAPI::instance().releaseResourcesFunc());
     }
-    ASSERT_TRUE(_called);
+    ASSERT_CALLED(MockAPI::instance().releaseResourcesFunc());
 }
-
--- tests/test_api.h
+++ tests/test_api.h
@@ -1,31 +1,148 @@
 #pragma once
 
-struct some_type_t {
-    int x;
-    int *data;
+#include <memory>
+#include <exception>
+
+namespace cwrap {
+
+namespace testing {
+
+namespace mock {
+
+class MockException : public std::exception {
+    public:
+        const char *what() const noexcept override {
+            return "The mock was instrumented incorrectly";
+        }
 };
 
-constexpr int TEST_ERR_INVAL = 1;
+class some_type_t {
+    public:
+
+        bool isFreed() const {
+            return freed && released;
+        }
+
+        void reset() {
+            freed = false;
+            released = false;
+        }
+
+        friend struct release_resources_operator;
+        friend struct free_resources_operator;
+        friend struct do_init_work_operator;
+        friend struct  create_and_initialize_operator;
+
+    private:
+        bool released = false;
+        bool freed = false;
+};
+
+static some_type_t __some_type;
 
 void do_init_work(some_type_t *ptr) {
-    if (nullptr == ptr) {
-        return;
-    }
-    ptr->x = 17;
-    ptr->data = new int;
-    *ptr->data = 19;
-}
-
-some_type_t *do_init_work_return() {
-    return new some_type_t;
-}
-
-void some_free_func(some_type_t *ptr) {
     if (ptr) {
-        if (ptr->data) {
-            delete ptr;
-            ptr->data = nullptr;
-        }
-        ptr->x = 0;
+        ptr->reset();
     }
+}
+
+some_type_t *create_and_initialize() {
+    do_init_work(&__some_type);
+    return &__some_type;
+}
+
+void release_resources(some_type_t *ptr);
+void free_resources(some_type_t *ptr);
+
+struct __mock_call_operator {
+    public:
+        bool called() const {
+            return _called;
+        }
+    protected:
+        bool _called { false };
+};
+
+struct release_resources_operator : public __mock_call_operator {
+    void operator() (some_type_t *ptr) {
+        ptr->released = true;
+        _called = true;
+    }
+};
+
+struct free_resources_operator : public __mock_call_operator {
+    void operator() (some_type_t *ptr) {
+        if (ptr != &__some_type) {
+            throw MockException {};
+        }
+        release_resources(ptr);
+        ptr->freed = true;
+        _called = true;
+    }
+};
+
+class MockAPI {
+    public:
+        const free_resources_operator &freeResourcesFunc() const {
+            return _freeFunc;
+        }
+
+        const release_resources_operator &releaseResourcesFunc() const {
+            return _releaseFunc;
+        }
+
+        void reset() {
+            _freeFunc = free_resources_operator {};
+            _releaseFunc = release_resources_operator {};
+        }
+
+        void doFreeResources(some_type_t *ptr) {
+            _freeFunc(ptr);
+        }
+
+        void doReleaseResources(some_type_t *ptr) {
+            _releaseFunc(ptr);
+        }
+
+        static MockAPI &instance() {
+            return *_instance;
+        }
+
+    private:
+        free_resources_operator _freeFunc;
+        release_resources_operator _releaseFunc;
+
+        static std::unique_ptr<MockAPI> _instance;
+};
+
+std::unique_ptr<MockAPI> MockAPI::_instance {std::make_unique<MockAPI>()};
+
+void free_resources(some_type_t *ptr) {
+    MockAPI::instance().doFreeResources(ptr);
+}
+
+void release_resources(some_type_t *ptr) {
+    MockAPI::instance().doReleaseResources(ptr);
+}
+
+}
+
+namespace assertions {
+
+template <class T>
+void ASSERT_CALLED(T &&t) {
+    ASSERT_TRUE(t.called());
+}
+
+template <class T>
+void ASSERT_NOT_CALLED(T &&t) {
+    ASSERT_FALSE(t.called());
+}
+
+}
+
+}
+
+
+
 }
